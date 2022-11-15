b51355aba208530e56d63a60a5dd90034c8cd262
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-15 15:04:37 +0500

    Rearrange the functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './style.css';
 import ItemDetails from './modules/object.js';
 // eslint-disable-next-line import/no-cycle
-import CRUD from './modules/CRUD.js';
+import {
+  updateListField, deleteItem, updateIndex, refresh,
+} from './modules/functions.js';
 
 // grab the element
 const form = document.querySelector('#form');
+const toDoList = document.querySelector('.toDoList');
+const clearTask = document.querySelector('.clearTask');
 
-// declear and empty array to store objects
 // eslint-disable-next-line prefer-const, import/no-mutable-exports
 let itemList = [];
 
@@ -17,6 +20,76 @@ const storeData = () => {
 
 const clearInputField = () => {
   form.toDoTitle.value = '';
+};
+
+// CRUD STAND FOR Create Read Update Delete
+
+const CRUD = (newItem, indexNum) => {
+  const itemWrapper = document.createElement('div');
+  itemWrapper.className = 'itemWrapper';
+  const li = document.createElement('li');
+  li.className = 'toDO';
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.className = 'box';
+  const listInput = document.createElement('label');
+  listInput.id = 'description';
+  listInput.innerHTML = newItem;
+  /// Icons
+  const dotIcon = document.createElement('i');
+  dotIcon.className = 'fas fa-ellipsis-vertical';
+  const trashIcon = document.createElement('i');
+  trashIcon.className = 'fas fa-trash-alt';
+  // -----------------------------------
+  itemWrapper.appendChild(checkBox);
+  itemWrapper.appendChild(listInput);
+  li.appendChild(itemWrapper);
+  li.appendChild(dotIcon);
+  li.appendChild(trashIcon);
+  toDoList.appendChild(li);
+
+  dotIcon.addEventListener('click', () => {
+    dotIcon.style.display = 'none';
+    trashIcon.style.display = 'block';
+  });
+
+  checkBox.addEventListener('click', () => {
+    if (checkBox.checked === true) {
+      listInput.style.textDecoration = 'line-through';
+      listInput.style.textDecorationColor = '#000';
+      listInput.style.textDecorationThickness = '4px';
+      itemList[indexNum].completed = true;
+      storeData();
+    } else {
+      itemList[indexNum].completed = false;
+      listInput.style.textDecoration = 'none';
+      storeData();
+    }
+  });
+
+  if (itemList[indexNum].completed === true) {
+    checkBox.checked = true;
+    listInput.style.textDecoration = 'line-through';
+    listInput.style.textDecorationColor = '#000';
+    listInput.style.textDecorationThickness = '4px';
+  } else {
+    checkBox.checked = false;
+  }
+
+  li.onclick = () => {
+    listInput.contentEditable = 'true';
+  };
+
+  listInput.addEventListener('keyup', (e) => {
+    if ((e.target.id === 'description')) {
+      updateListField(e.target, indexNum);
+    }
+  });
+
+  trashIcon.addEventListener('click', (el) => {
+    el.target.parentElement.remove();
+    deleteItem(indexNum);
+  });
 };
 
 const renderItem = (dataList) => {
@@ -33,9 +106,16 @@ const getStoreData = () => {
     renderItem(previousData);
   }
 };
-// ------------------------
 
 getStoreData();
+
+// clear all task which are completed
+clearTask.addEventListener('click', () => {
+  itemList = itemList.filter((item) => item.completed === false);
+  updateIndex();
+  storeData();
+  refresh();
+});
 
 // add event listenser to the form
 form.addEventListener('submit', (e) => {
