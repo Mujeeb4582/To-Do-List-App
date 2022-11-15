756486f28f90e866b3ca6c68392ad49646aeb651
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-15 12:26:03 +0500

    Add the form validation feature

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@
 const form = document.querySelector('#form');
 const toDoList = document.querySelector('.toDoList');
 
+// functions --------------
+const clearInputField = () => {
+  form.toDoTitle.value = '';
+};
+// ------------------------
+
 // add event listenser to the form
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const item = form.toDoTitle.value.trim();
+  // form Validation if field is empty or just enter spaces
+  // grab main element
+  const main = document.querySelector('main');
+  // grab first child of main element
+  const { firstChild } = main;
+  if (item === '') {
+    // create new element
+    const msg = document.createElement('small');
+    msg.innerHTML = 'This field is required and cannot be empty!';
+    main.insertBefore(msg, firstChild);
+    setTimeout(() => {
+      msg.style.display = 'none';
+      clearInputField();
+    }, 2500);
+  }
 });
