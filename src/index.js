c8500ca5546d8a4766cf482717b4674b43ae3cbc
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-15 12:15:26 +0500

    Get the value from user

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ const toDoList = document.querySelector('.toDoList');
 // add event listenser to the form
 form.addEventListener('submit', e => {
   e.preventDefault();
+
+  const item = form.toDoTitle.value.trim();
 });
